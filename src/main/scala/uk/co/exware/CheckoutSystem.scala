d00c62cf4397394f6236ead562b73ec26a7ac591
Aaron East <eastaa@visa.com>
2015-09-17 17:16:11 +0200

    Moved everything into on CheckoutSystem file

--- src/main/scala/uk/co/exware/CheckoutSystem.scala
+++ src/main/scala/uk/co/exware/CheckoutSystem.scala
@@ -1,5 +1,38 @@
 package uk.co.exware
 
+import scala.collection.mutable.ListBuffer
+
+class ShoppingCart {
+
+  var items = new ListBuffer[String]()
+
+  def addItem(item:String) :Unit = {
+    items += item
+  }
+
+  def getAllItems() :List[String] = {
+    return items.toList
+  }
+}
+
+case class PricedItem(name:String, price:BigDecimal)
+
+class Till {
+
+  def scan(items: List[String]): List[PricedItem] = {
+    return items.map(i => createPricedItem(i)).filter(o => o.isDefined).flatten
+  }
+
+  def createPricedItem(item:String): Option[PricedItem] = {
+    return item match {
+      case "Apple" => Option(PricedItem("Apple", BigDecimal("0.60")))
+      case "Orange" => Option(PricedItem("Orange", BigDecimal("0.25")))
+      case _ => Option.empty
+    }
+  }
+
+}
+
 object CheckoutSystem {
 
   def price(pricedItems: List[PricedItem]): BigDecimal = {
--- src/test/scala/uk/co/exware/CheckoutSystemSpec.scala
+++ src/test/scala/uk/co/exware/CheckoutSystemSpec.scala
@@ -2,6 +2,38 @@ package uk.co.exware
 
 import org.scalatest.{Matchers, FlatSpec}
 
+class ShoppingCartSpec extends FlatSpec with Matchers {
+
+  val underTest = new ShoppingCart()
+
+  "A shopping cart" should "be able to add an apple to the shopping cart" in {
+    underTest.addItem("Apple")
+
+    val results = underTest.getAllItems()
+
+    results.size shouldEqual 1
+    results.head shouldEqual "Apple"
+  }
+}
+
+class TillSpec extends FlatSpec with Matchers{
+
+  val underTest = new Till()
+
+  "A Till" should "create a list of priced items" in {
+
+    val items: List[String] = List("Apple", "Apple", "Orange")
+
+    val results: List[PricedItem] = underTest.scan(items)
+
+    print(results)
+
+    results.size shouldEqual 3
+
+  }
+
+}
+
 class CheckoutSystemSpec extends FlatSpec with Matchers{
 
   val underTest = CheckoutSystem
